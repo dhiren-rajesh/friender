da40a5e6591a175d62692dba671c48afd4b8c50b
Dhiren Rajesh <dhirenrajeshk@gmail.com>
2021-08-22 22:02:18 +0530

    Added Header Icon-Buttons

---
+++ src/Header/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Header.css'
+import IconButton from "@material-ui/core/IconButton";
+import PersonIcon from '@material-ui/icons/Person';
+import GroupWorkIcon from '@material-ui/icons/GroupWork';
+import ForumIcon from '@material-ui/icons/Forum';
+
+export default function Header() {
+    var styles = {fontSize: "2.5rem"}
+    return (
+        <div className="header">
+            <IconButton>
+                <PersonIcon style={styles} className="header-icon"/>
+            </IconButton>
+            <IconButton>
+                <GroupWorkIcon style={{color: "#E63E6D", fontSize: "2.5rem"}}/>
+            </IconButton>
+            <IconButton>
+                <ForumIcon style={styles} className="header-icon"/>
+            </IconButton>
+        </div>
+    )
+}
